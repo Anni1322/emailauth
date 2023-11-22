a427d2048a5473bc0d076da747fbdce86cde84c6
anil wasisth <77065260+Anni1322@users.noreply.github.com>
2023-11-22 23:16:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+require('dotenv').config();
+const mongoose= require('mongoose');
+const express = require('express');
+const app = express();
+
+ 
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+  });
+
+// for user routes
+const userRoute = require('./routes/userRoute')
+app.use('/',userRoute);
+
+// for admin routes
+const adminRoute = require('./routes/adminRoute')
+app.use('/admin',adminRoute);
+
+app.listen(PORT,function () {
+    console.log("server is runnig......");
+    
+})
